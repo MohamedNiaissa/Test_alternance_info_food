e65d2705f73aafc9e3ea22a7d872fd8aa3762bb6
Mohamed <mohameddrame907@gmail.com>
2022-08-05 00:30:24 +0200

    travail de design en cours

--- js/details.js
+++ js/details.js
@@ -13,14 +13,15 @@ let paraDeux = document.createElement("p");
 
 let detailsItem = document.querySelector(".detailsItem")
 
-paraUn.innerHTML = localStorage.getItem("categories");
-paraDeux.innerHTML = localStorage.getItem("Ancien packaging")
+paraUn.innerHTML = "Catégories : " + localStorage.getItem("categories");
+paraDeux.innerHTML = "Ancien packaging : " + localStorage.getItem("Ancien packaging")
 
 desc.appendChild(paraUn);
 desc.appendChild(paraDeux);
 
 let paraLorem = document.createElement("p");
 paraLorem.innerHTML = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet dolores fugiat temporibus assumenda aliquam modi eum quisquam dicta nisi repellendus perferendis iure qui vero doloremque libero, magnam laborum nesciunt maxime.Laboriosam enim rerum repellat in accusamus libero, dolor deserunt, soluta praesentium odio veritatis quasi incidunt laborum doloribus laudantium labore similique? Delectus voluptates impedit perspiciatis qui aliquam! Consequuntur eveniet nisi neque.Eaque est, voluptatum nam qui quasi voluptatem aspernatur molestiae ducimus incidunt earum iusto placeat sed alias debitis maxime possimus dolores consequatur itaque! Doloribus sint quibusdam enim provident sunt? Saepe, praesentium!";
+paraLorem.style.textAlign = "center";
 
 detailsItem.appendChild(paraLorem);
 
